6b5775e804c731b7cffdfd61745852e1e93fb22c
Carl-Stanley <cds.beml@gmail.com>
2019-11-05 10:44:32 -0700

    COmmit

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ server.get('/',(req, res) => {
     res.send('Hello World!');
 });
 
+server.get('/hobbits',(req, res) => {
+    const hobbits = [
+        {
+        id: 1,
+        name: 'Samwise Gamgee'
+        },
+        {
+        id: 2,
+        name: 'Frodo Baggins'
+        },
+        {
+        id: 3,
+        name: 'Bilbo Baggins'
+        }
+    ]
+});
+
 server.listen(8000,() => console.log('API Running on port 8000'));
