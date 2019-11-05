d1aac6096e9cde454d3a9ef19367fc1b65f20f29
Carl-Stanley <cds.beml@gmail.com>
2019-11-05 11:29:23 -0700

    Commit - Adding Crud operations.

--- index.js
+++ index.js
@@ -3,26 +3,43 @@ const express = require('express');
 
 const server = express();
 
+const hobbits = [
+    {
+    id: 1,
+    name: 'Samwise Gamgee'
+    },
+    {
+    id: 2,
+    name: 'Frodo Baggins'
+    },
+    {
+    id: 3,
+    name: 'Bilbo Baggins'
+    }
+]
 server.get('/',(req, res) => {
     res.send('Hello World!');
 });
 
+// Get all of the hobbits 
 server.get('/hobbits',(req, res) => {
-    const hobbits = [
-        {
-        id: 1,
-        name: 'Samwise Gamgee'
-        },
-        {
-        id: 2,
-        name: 'Frodo Baggins'
-        },
-        {
-        id: 3,
-        name: 'Bilbo Baggins'
-        }
-    ]
-    res.status(200).json(hobbits);
+    
+    res.send('Welcome to Hobbiton');
+    });
+// Add a Hobbit
+server.post('/hobbits',(req, res) => {
+    
+    res.status(201).json({url: '/hobbits', operation: 'POST'});
+    });
+// Update a Hobbit
+server.put('/hobbits',(req, res) => {
+    
+    res.status(201).json({url: '/hobbits', operation: 'PUT'});
+    });
+// Delete a Hobbit
+server.delete('/hobbits',(req, res) => {
+    
+    res.status(204);
     });
 
 server.listen(8000,() => console.log('API Running on port 8000'));
