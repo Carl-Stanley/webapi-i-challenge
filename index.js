c273dc81b8e7c1cf25f3675855472d8ade61e8fd
Carl-Stanley <cds.beml@gmail.com>
2019-11-05 15:19:44 -0700

    Commit

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ const hobbits = [
     }
 ]
 
+//server.use(function(req, res) {
+  //  res.status(404).send('Dead End....');
+//});
+
 server.use(express.json());
 
 server.get('/',(req, res) => {
@@ -43,6 +47,11 @@ server.put('/hobbits',(req, res) => {
 server.delete('/hobbits',(req, res) => {
     
     res.sendStatus(204);
+    });
+
+
+server.use(function(req, res) {
+    res.status(404).send('Dead End....');
     });
 
 server.listen(8000,() => console.log('API Running on port 8000'));
